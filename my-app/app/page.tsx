b657fa85578e077650e9d20169288744ff29eefe
elaa <elaa.brahmii@gmail.com>
2025-05-24 13:34:28 +0100

    building header and footer

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,15 @@
 //import Image from "next/image";
-
+import {Button} from "@/components/ui/button"
 export default function Home() {
   return (
     <div>
-      <h1 className="text-center text-4xl font-bold">sommaire</h1>
+      <h1 className="grid grid-rows-[20px_1fr_20px]
+      items-center justify-items-center min-h-screen
+      p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-sans)]
+      
+      ">
+        sommaire</h1>
+        <Button variant="outline" size={'lg'}></Button>
     </div>
   );
 }
---
+++ my-app/components/common/header.tsx
@@ -0,0 +1,5 @@
+export default function Header(){
+    return <nav className="container">
+
+    </nav>
+}
