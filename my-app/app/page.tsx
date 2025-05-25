6325e27a06fd919973ccb3e6e59a3118a403d5bb
elaa <elaa.brahmii@gmail.com>
2025-05-25 12:20:34 +0100

    implemented bg gradient

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,13 @@
 //import Image from "next/image";
 import HeroSection from '@/components/home/HeroSection'
+import BgGradient from '@/components/common/BgGradient'
 export default function Home() {
   return (
     <div className="relative w-full">
-      <HeroSection/>
+      <BgGradient/>
+      <div className="flex flex-col">
+        <HeroSection/>
+      </div>
      {/*  <DemoSection/>
       <HowItWorksSection/>
       <Pricing/>
