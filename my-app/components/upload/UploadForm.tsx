049bb9d9cbdd8951055b2a06bb135734ea88b383
elaa <elaa.brahmii@gmail.com>
2025-05-26 11:41:54 +0100

    added zod for schema validation

--- my-app/components/upload/UploadForm.tsx
+++ my-app/components/upload/UploadForm.tsx
@@ -1,12 +1,26 @@
 "use client"
 import UploadFormInput from '@/components/upload/upload-form-input';
+import { z } from 'zod';
+
+const schema=z.object({});
 
 export default function UploadForm() {
-    const handleSubmit=()=>{
+    const handleSubmit=(e:React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
         console.log("submitted");
+        const formData=new FormData(e.currentTarget);
+        const file=formData.get('file') as File;
+        //validation the fields
+        //schema validation with zod
+        //upload file to the uploadThing
+        //parse the pdf using lang chain
+        //summarize the pdf using ai
+        //save the summary to db
+        //redirect to the [id] summary page
+
     }
     return(
-        <div>
+        <div className="flex flex-col gap-8 max-w-2xl mx-auto">
           <UploadFormInput onSubmit={handleSubmit}/>
         </div>
     )
--- my-app/components/upload/upload-form-input.tsx
+++ my-app/components/upload/upload-form-input.tsx
@@ -1,5 +1,6 @@
 "use client"
 import {Button} from '@/components/ui/button'
+import { Input } from '../ui/input';
 interface UploadFormProps{
     onSubmit:(e:React.FormEvent<HTMLFormElement>)=>void;
 
@@ -8,8 +9,12 @@ export default function UploadFormInput({onSubmit}:UploadFormProps)
     {
     return(
         <form className="flex flex-col gap-6" onSubmit={onSubmit}>
-        <input type="file"/>
-        <Button>Upload your PDF</Button>
+            <div className="flex justify-end gap-1.5">
+        <Input type="file" id="file" name="file"
+        accept="application/pdf"
+        required
+        className=""/>
+        <Button>Upload your PDF</Button></div>
 
         </form>
         )
