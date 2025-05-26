7447d550ca62c7bf9d3b16a34e732c9c235564c0
elaa <elaa.brahmii@gmail.com>
2025-05-26 11:53:38 +0100

    going through validation process of the file

--- my-app/components/upload/UploadForm.tsx
+++ my-app/components/upload/UploadForm.tsx
@@ -2,8 +2,16 @@
 import UploadFormInput from '@/components/upload/upload-form-input';
 import { z } from 'zod';
 
-const schema=z.object({});
-
+const schema=z.object({
+    file:z.instanceof(File,{message:'invalid file'}).
+    refine((file)=>file.size<=20*1024*1024,
+        'file size must be less than 20MB' //we don't need a message
+    )
+    .refine((file)=>file.type.startsWith('application/pdf'),
+    'file must b a pdf'
+),
+}); //file object
+ 
 export default function UploadForm() {
     const handleSubmit=(e:React.FormEvent<HTMLFormElement>)=>{
         e.preventDefault();
@@ -11,7 +19,15 @@ export default function UploadForm() {
         const formData=new FormData(e.currentTarget);
         const file=formData.get('file') as File;
         //validation the fields
+        const validatedFields=schema.safeParse({file});
         //schema validation with zod
+        if(!validatedFields.success){
+            console.log(validatedFields.error.flatten().fieldErrors.file?.
+            [0] ?? 'invalid file'
+        
+        );
+
+        }
         //upload file to the uploadThing
         //parse the pdf using lang chain
         //summarize the pdf using ai
