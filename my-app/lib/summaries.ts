eea4bf5ab4c5680d0c9e0c662b8144ea6d66538d
elaa <elaa.brahmii@gmail.com>
2025-05-30 19:30:09 +0100

    done

--- my-app/lib/summaries.ts
+++ my-app/lib/summaries.ts
@@ -3,17 +3,7 @@ export async function getSummaries(userId:string){
     
     try{
         const sql=await getDbConnection();
-        const summaries=await sql`SELECT id,
-        user_id,
-        title,
-        original_file_url,
-        summary_text,
-        word_count,
-        created_at,
-        updated_at,
-        status,
-        file_name,
-        LENGTH(summary_text)-LENGTH(REPLACE(summary_text,' ','')) +1 as word_count
+        const summaries=await sql`SELECT *
         FROM pdf_summaries where user_id=${userId} 
         ORDER BY created_at DESC `;
         return summaries;
--- my-app/lib/summary.ts
+++ my-app/lib/summary.ts
@@ -2,7 +2,16 @@ import {getDbConnection} from '@/lib/db'
 export async function getSummaryById(id: string) {
     try{
     const sql = await getDbConnection();
-    const [summary] = await sql`SELECT * FROM pdf_summaries where id=${id}`;
+    const [summary] = await sql`SELECT id,
+        user_id,
+        title,
+        original_file_url,
+        summary_text,  
+        created_at,
+        updated_at,
+        status,
+        file_name,
+        LENGTH(summary_text)-LENGTH(REPLACE(summary_text,' ','')) +1 as word_count FROM pdf_summaries where id=${id}`;
   
     
     return summary;}
