f24a268cec3f1f864e461e26078ae1455a7c6993
elaa <elaa.brahmii@gmail.com>
2025-05-30 14:13:17 +0100

    completed fetching summary by id

--- my-app/lib/db.ts
+++ my-app/lib/db.ts
@@ -1,10 +1,11 @@
 // app/actions.ts
 "use server";
-import { neon, NeonQueryFunction } from "@neondatabase/serverless";
+import { neon } from "@neondatabase/serverless";
 
-export async function getDbConnection(): Promise<NeonQueryFunction<false, false>> {
+export async function getDbConnection() {
     if(!process.env.DATABASE_URL){
         throw new Error('neon database url is not defined');
+        return;
     }
     return neon(process.env.DATABASE_URL);
 }
