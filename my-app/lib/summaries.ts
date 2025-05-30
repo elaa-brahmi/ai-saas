448df2ce69ed139d315e5931e41b8e1d1662096d
elaa <elaa.brahmii@gmail.com>
2025-05-30 18:24:57 +0100

    edited the fetch summaries query to include word count

--- my-app/lib/summaries.ts
+++ my-app/lib/summaries.ts
@@ -3,7 +3,19 @@ export async function getSummaries(userId:string){
     
     try{
         const sql=await getDbConnection();
-        const summaries=await sql`SELECT * FROM pdf_summaries where user_id=${userId} ORDER BY created_at DESC `;
+        const summaries=await sql`SELECT id,
+        user_id,
+        title,
+        original_file_url,
+        summary_text,
+        word_count,
+        created_at,
+        updated_at,
+        status,
+        file_name,
+        LENGTH(summary_text)-LENGTH(REPLACE(summary_text,' ','')) +1 as word_count
+        FROM pdf_summaries where user_id=${userId} 
+        ORDER BY created_at DESC `;
         return summaries;
 
     
