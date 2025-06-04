e2d1b998780d533900c9a938aeda8a55358937a9
elaa <elaa.brahmii@gmail.com>
2025-06-04 12:48:31 +0100

    footer done

--- my-app/components/common/footer.tsx
+++ my-app/components/common/footer.tsx
@@ -1,5 +1,12 @@
 export default function Header(){
     return(
-        <div>footer</div>
+        <footer className="bg-linear-to-r from-rose-50 to-rose-50 text-center text-lg">
+       
+        <div className="text-center p-3 bg-black/5">
+          © 2025 Copyright:{' '}
+          <a className="text-gray-800 hover:underline" href="#"><span className="text-xl text-rose-700 font-bold underline ms-1">{' '}Sommaire AI</span></a>
+        </div>
+       
+      </footer>
     )
 }
